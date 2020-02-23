6987d5a9b87fc4d174288fcfc23fec472ef86a4a
Matthew Gleich <matthewgleich@gmail.com>
2020-02-22 23:45:58 -0500

    Restart application writing in dart

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Drive_Move/Drive_Move.dart' as Drive_Move;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Drive_Move.calculate()}!');
+}
---
+++ test/Drive_Move_test.dart
@@ -0,0 +1,8 @@
+import 'package:Drive_Move/Drive_Move.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
