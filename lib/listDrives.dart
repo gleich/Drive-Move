85e3aa6e2343d41b84b34053de6ad61e54da2fd6
Matthew Gleich <matthewgleich@gmail.com>
2020-02-24 21:34:11 -0500

    Update listDrives.dart

---
+++ lib/listDrives.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+class ListDrives {
+  static List<String> _getMacOSDrives(List<String> ignoredDrives) {
+    var validDrives = <String>[];
+    var volumesContent = Directory('/Volumes').listSync();
+    for (var fileOrDir in volumesContent) {
+      var cleanedPath = fileOrDir.path.toString().replaceAll('/Volumes/', '');
+      if (fileOrDir is Directory &&
+          ignoredDrives.contains(cleanedPath) == false) {
+        validDrives.add(cleanedPath);
+      }
+    }
+    return validDrives;
+  }
+
+  static List<String> listDrives(List<String> ignoredDrives) {
+    final platform = Platform.operatingSystem;
+    switch (platform) {
+      case 'macos':
+        {
+          return _getMacOSDrives(ignoredDrives);
+        }
+      default:
+        {
+          print('Not supported for $platform');
+          exit(1);
+        }
+    }
+  }
+}
+
+void main() {
+  print(ListDrives.listDrives([]));
+}
--- test/Drive_Move_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:Drive_Move/Drive_Move.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
