952cd2b98d63e3ae3014be6ba03621c1549a92b1
Matthew Gleich <matthewgleich@gmail.com>
2020-03-04 23:39:20 -0500

    Initialize configuration

--- lib/configFile.dart
+++ lib/configFile.dart
@@ -4,6 +4,8 @@ class ConfigFile {
   static Map initConfig() {
     final platform = Platform.operatingSystem;
     List<String> defaultIgnoredDrives;
+    String configPath;
+    // Setting defaultIgnoredDrives and file path to config file
     switch (platform) {
       case 'macos':
         {
@@ -12,16 +14,32 @@ class ConfigFile {
             'Macintosh HD',
             'com.apple.TimeMachine.localsnapshots'
           ];
+          configPath =
+              Directory.current.path.split('/').getRange(0, 3).join('/');
         }
     }
+    var lines = [
+      '# More info at https://github.com/Matt-Gleich/Drive-Move/blob/master/README.md',
+      'ignoredDrives:',
+    ];
     if (defaultIgnoredDrives.isNotEmpty) {
-      var lines = [
-        '# More info at https://github.com/Matt-Gleich/Drive-Move/blob/master/README.md',
-        'ignoredDrives:'
-      ];
-      for (var ignoredDrive in defaultIgnoredDrives) {
-        lines.add('  - $ignoredDrive');
+      for (var drive in defaultIgnoredDrives) {
+        lines.add('  - $drive');
       }
+    }
+    // Crating and writing template to ~/.driveMove/config.yml
+    configPath = configPath + '/.driveMove/config.yml';
+    print(configPath.runtimeType);
+    if (FileSystemEntity.typeSync(configPath) !=
+        FileSystemEntityType.notFound) {
+      print("deleting $configPath");
+      File(configPath).delete(recursive: true);
+    }
+    File(configPath).create(recursive: true);
+    var file = File(configPath);
+    var sink = file.openWrite(mode: FileMode.append);
+    for (var line in lines) {
+      sink.write('$line\n');
     }
   }
 }
